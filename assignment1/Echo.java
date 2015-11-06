e965eabdad66f55adbe17a635e08025cecf42364
Dan Benedikt Drewes <drewes@campus.tu-berlin.de>
2015-11-06 11:51:23 +0100

    echo implemented without highlighting

---
+++ assignment1/Echo.java
@@ -0,0 +1,47 @@
+import teachnet.algorithm.BasicAlgorithm;
+import java.awt.Color;
+
+
+public class Echo extends BasicAlgorithm{
+	
+	boolean informed = false;
+	boolean initiator = false;
+	int activator;
+	int count = 0;
+	int markInterface =-1;
+	
+	
+	
+	public void initiate(){
+		for (int i = 0; i < checkInterfaces(); i++) {
+			send(i, "Explorer");
+		}
+		informed=true;
+		initiator=true;
+	}
+	
+	public void receive(int interf, Object message){
+			if(!informed){
+				for (int i = 0; i < checkInterfaces(); i++) { //flood
+					if(i != interf){
+						send(i, "Explorer");
+					}			
+				}
+				informed=true;
+				activator=interf; //remember activator
+				markInterface=activator;
+			}			
+			count++;
+			if(count==checkInterfaces()){ //all neighbours informed
+				if(!initiator){
+					send(activator,"Echo");
+				}
+				//no explicit exit possible in teachnet
+			}
+
+	}
+	
+	
+	
+	
+}
