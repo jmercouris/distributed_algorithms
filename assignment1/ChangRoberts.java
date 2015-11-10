2e265b893e7e486d18a00184b4b03ca8226143a6
Manuela Hopp <mutex42@users.noreply.github.com>
2015-11-10 19:50:26 +0100

    some cleaning up. does anybody know how a node can find out what its id is?

--- assignment1/ChangRoberts.java
+++ assignment1/ChangRoberts.java
@@ -6,28 +6,28 @@ public class ChangRoberts extends BasicAlgorithm {
   int id;
 
   public void initiate(){
-	Mp = id;
-	for (int i = 0; i < checkInterfaces(); i++) {
-	  send(i, Mp);
-	}
+    Mp = id;
+    for (int i = 0; i < checkInterfaces(); i++) {
+      send(i, Mp);
+    }
   }
   
   public void receive(int interf, Object message){
-	if (message.equals("I am the master")); // then we are done
-	else {
-	  int k = (int) message;
-	  if (Mp < k){
-		Mp = k;
-		for (int i = 0; i < checkInterfaces(); i++){
-		  send(i, Mp);
-		}
-	  } else {
-		if (j == Mp){
-		  for (int i = 0; i < checkInterfaces(); i++){
-			send(i,"I am the master");
-		  }
-		}
-	  }
-	}
+    if (message.equals("I am the master")); // then we are done
+    else {
+      int k = (int) message;
+      if (Mp < k){ // received higher id, forward it to all neighbors
+        Mp = k;
+        for (int i = 0; i < checkInterfaces(); i++){
+          send(i, Mp);
+        }
+      } else {
+        if (j == Mp){
+          for (int i = 0; i < checkInterfaces(); i++){ // got own id, declare master
+            send(i, "I am the master");
+          }
+        }
+      }
+    }
   }
 }
