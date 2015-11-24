bd4b9ecc4e25c02aaad9a6098098f0d94dff737c
John Mercouris <jmercouris@gmail.com>
2015-11-24 14:53:01 +0100

    Working on Three phase election algorithm

--- assignment2/ThreePhaseElection.java
+++ assignment2/ThreePhaseElection.java
@@ -1,51 +1,62 @@
 import teachnet.algorithm.BasicAlgorithm;
 
 /**
-* Group 11
-* Dan Drewes, Manuela Hopp, John Mercouris, Malte Siemers
-* This class implements the Echo algorithm
-*/
-public class ThreePhaseElection extends BasicAlgorithm{
+ * Group 11
+ * Dan Drewes, Manuela Hopp, John Mercouris, Malte Siemers
+ * This class implements the ThreePhaseElection algorithm
+ */
+public class ThreePhaseElection extends BasicAlgorithm {
 
-  boolean informed = false;
-  boolean initiator = false;
-  int activator;
-  int count = 0;
-  int markInterface =-1;
+    String caption;
+    boolean informed = false;
+    boolean initiator = false;
+    int id;
+    int activator;
+    int value;
+    int count = 0;
+    int markInterface =-1;
 
-  /**
-  * initiates the algorithm;
-  * sends an explorer message to all neighbors and sets informed and initiator to true
-  */
-  public void initiate(){
-    for (int i = 0; i < checkInterfaces(); i++) {
-      send(i, "Explorer");
+    /**
+     * setup function
+     */
+    public void setup(java.util.Map<String, Object> config){
+	id = (Integer) config.get("node.id");
+	caption = ""+id;
     }
-    informed=true;
-    initiator=true;
-  }
 
-  /**
-  * sends out explorer messages to all neighbors if not yet informed
-  * sends echo to activator if all neighbors are informed
-  */
-  public void receive(int interf, Object message){
-    if(!informed){
-      for (int i = 0; i < checkInterfaces(); i++) { //flood
-        if(i != interf){
-           send(i, "Explorer");
-        }
-      }
-      informed = true;
-      activator = interf; // remember activator
-      markInterface = interf; // highlight spanning tree
+    /**
+     * initiates the algorithm;
+     * sends an explorer message to all neighbors and sets informed and initiator to true
+     */
+    public void initiate(){
+	for (int i = 0; i < checkInterfaces(); i++) {
+	    send(i, "Explorer");
+	}
+	informed=true;
+	initiator=true;
     }
-    count++;
-    if(count == checkInterfaces()){ //all neighbours informed
-      if(!initiator){
-        send(activator,"Echo");
-      }
-      //no explicit exit possible in teachnet
+
+    /**
+     * sends out explorer messages to all neighbors if not yet informed
+     * sends echo to activator if all neighbors are informed
+     */
+    public void receive(int interf, Object message){
+	if(!informed){
+	    for (int i = 0; i < checkInterfaces(); i++) { //flood
+		if(i != interf){
+		    send(i, "Explorer");
+		}
+	    }
+	    informed = true;
+	    activator = interf; // remember activator
+	    markInterface = interf; // highlight spanning tree
+	}
+	count++;
+	if(count == checkInterfaces()){ //all neighbours informed
+	    if(!initiator){
+		send(activator,"Echo");
+	    }
+	    //no explicit exit possible in teachnet
+	}
     }
-  }
 }
