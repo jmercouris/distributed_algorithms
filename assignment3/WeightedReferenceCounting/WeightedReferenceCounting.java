402073aa444fad50e28e0e1d17f6e1ed9fb085e1
John Mercouris <jmercouris@gmail.com>
2015-12-10 12:42:48 +0100

    Random requesting intervals

--- assignment3/WeightedReferenceCounting/WeightedReferenceCounting.java
+++ assignment3/WeightedReferenceCounting/WeightedReferenceCounting.java
@@ -1,4 +1,5 @@
 import teachnet.algorithm.BasicAlgorithm;
+import java.util.Random;
 
 
 /**
@@ -10,33 +11,44 @@ public class WeightedReferenceCounting extends BasicAlgorithm{
     int id; // ID of the node
     boolean informed = false; // Recieved explorer message
     String caption = ""; // String next to node
+    Random generator = new Random(); // Random number generator, used for control
+    int intervalDelayRange = 10;
 
+    ////////////////////////////////////////////////////////////////////////////////
     // Setup Function
+    ////////////////////////////////////////////////////////////////////////////////
     public void setup(java.util.Map<String, Object> config){
 	id = (Integer) config.get("node.id");
 	caption = "" + id;
-
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
     // Initiate the Algorithm by awakening all nodes
+    ////////////////////////////////////////////////////////////////////////////////
     public void initiate() {
 	sendAll(new NetworkMessage(NetworkMessage.EXPLORE));
-
-	// double delayTime = Math.random();
-	setTimeout(2, new Object());
-	
+	// Initialize independent node loops
+	setTimeout(0, new Object());
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
     // Handle timeouts
+    ////////////////////////////////////////////////////////////////////////////////
     public void timeout(Object inputObject) {
-	System.out.println("Timeout callback!");
-	sendAll(new NetworkMessage("Hey!"));
-	if (!informed) {
-	    setTimeout(2, new Object());
-	}
+	setTimeout(generator.nextInt(intervalDelayRange), new Object());
+	run();
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // Run Method
+    ////////////////////////////////////////////////////////////////////////////////
+    public void run() { 
+	sendAll(new NetworkMessage("Hey!"));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
     // Act on Recieved Messages
+    ////////////////////////////////////////////////////////////////////////////////
     public void receive(int sendingInterface, Object message) {
 	// Cast Message to Network Message Object
 	NetworkMessage inputMessage = (NetworkMessage) message;
