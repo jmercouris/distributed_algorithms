58c2317a0384effb2578eccd29a2bda014fc30e8
John Mercouris <jmercouris@gmail.com>
2015-12-10 15:45:52 +0100

    Working on returning requested references

--- assignment3/WeightedReferenceCounting/WeightedObject.java
+++ assignment3/WeightedReferenceCounting/WeightedObject.java
@@ -3,6 +3,7 @@
  */
 public class WeightedObject {
     double weight = 0;
+    double partialWeight = 0;
     int id = 0;
     
     // Default Constructor
@@ -10,9 +11,17 @@ public class WeightedObject {
 
     }
 
+    // Constructor with weight, initially partial weight and total weight are equal
     public WeightedObject(double inputWeight, int inputID) {
 	weight = inputWeight;
+	partialWeight = weight;
 	id = inputID;
+    }
+
+    // Split self in half, and give new reference
+    public WeightedObjectReference getWeightedObjectReference() {
+	weight = weight / 2;
+	return new WeightedObjectReference(id, weight);
     }
 
     public double getWeight() { 
--- assignment3/WeightedReferenceCounting/WeightedObjectReference.java
+++ assignment3/WeightedReferenceCounting/WeightedObjectReference.java
@@ -1,9 +1,20 @@
 /*
  * 
  */
-public class WeightedObjectReference {
+public class WeightedObjectReference extends WeightedObject {
     // Default Constructor
     public WeightedObjectReference() {
 
     }
+
+    public WeightedObjectReference (int inputID, double inputWeight) {
+	id = inputID;
+	weight = inputWeight;
+    }
+
+    // Split self in half, and give new reference
+    public WeightedObjectReference getWeightedObjectReference() {
+	return super.getWeightedObjectReference();
+    }
+
 }
