9eb98b269cbfc5efcb2d65e7da13ebfe6350ebd9
John Mercouris <jmercouris@gmail.com>
2015-12-11 17:07:06 +0100

    Completely functional

--- assignment3/WeightedReferenceCounting/WeightedObject.java
+++ assignment3/WeightedReferenceCounting/WeightedObject.java
@@ -4,29 +4,35 @@ import java.lang.Math;
  * Class represents a weighted object
  */
 public class WeightedObject {
-    double weight = 0;
-    double prototypeWeight = 0;
-    int id = 0;
+    double weight = 0; // Represents the partial weight
+    double prototypeWeight = 0; // Represents the original object weight
+    int id = 0; // The ID of the object
     
-    // Default Constructor
+    /**
+     * Default Constructor
+     */ 
     public WeightedObject() {
 
     }
 
-    // Constructor with weight, initially partial weight and total weight are equal
+    /**
+     * Constructor with weight, initially partial weight and total weight are equal
+     */
     public WeightedObject(double inputWeight, int inputID) {
 	weight = inputWeight;
 	prototypeWeight = weight;
 	id = inputID;
     }
 
-    // Split self in half, and give new reference
+    /**
+     * Split self in half, and give new reference
+     */
     public WeightedObjectReference getWeightedObjectReference() {
 	// Initialize reference weight to 0
 	double referenceWeight = 0.0;
 
 	// If our weight is 2 or above, give half our weight
-	if (weight > 1) {
+	if (weight >= 2) {
 	    weight = weight / 2;
 	    referenceWeight = weight;
 	}
@@ -34,7 +40,7 @@ public class WeightedObject {
 	// If our weight is 1, 
 	// set referenceWeight = 1, thereby giving away that weight, 
 	// and add 2^10 to our weight for further references
-	if (weight == 1) {
+	else if (weight == 1) {
 	    prototypeWeight += Math.pow(2, 10);	    
 	    weight = Math.pow(2, 10);
 	    referenceWeight = 1;
@@ -43,6 +49,10 @@ public class WeightedObject {
 	return new WeightedObjectReference(id, referenceWeight);
     }
 
+    /**
+     * Returns weight to the reference so that it may discarded when the prototypeWeight (weight)
+     * and weight (partial weight) are equal
+     */ 
     public boolean returnReference (double inputWeight) {
 	weight += inputWeight;
 	if (weight == prototypeWeight) {
@@ -51,17 +61,23 @@ public class WeightedObject {
         return false;
     }
 
-    // Get the weight of the object
+    /**
+     * Get the weight of the object
+     */
     public double getWeight() { 
 	return weight;
     }
     
-    // Get the ID of the object
+    /** 
+     * Get the ID of the object
+     */
     public int getID() { 
 	return id;
     }
 
-    // Tostring to examine object
+    /**
+     * Tostring to examine object
+     */
     public String toString() {
 	return "Total Weight: " + prototypeWeight + " Partial Weight: " + weight;
     }
