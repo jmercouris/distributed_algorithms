cdbde6d1d392f3b2731c748d3f0a4022a1a418ec
John Mercouris <jmercouris@gmail.com>
2016-01-05 17:53:25 +0100

    Trivial progress

---
+++ assignment4/distributed_deadlock/ExclusiveResource.java
@@ -0,0 +1,13 @@
+/*
+ * Exclusive Resource can only have one instance of self
+ */
+public class ExclusiveResource {
+    int id;
+    public ExclusiveResource() {
+
+    }
+    public ExclusiveResource(int inputId) {
+        id = inputId;
+    }
+    
+}
