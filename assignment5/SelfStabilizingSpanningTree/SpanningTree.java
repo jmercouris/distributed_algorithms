5bf8f12974ae3e694d04efc9cfd86b734122e6ab
John Mercouris <jmercouris@gmail.com>
2016-01-19 23:07:17 +0100

    STABLE

--- assignment5/SelfStabilizingSpanningTree/SpanningTree.java
+++ assignment5/SelfStabilizingSpanningTree/SpanningTree.java
@@ -14,11 +14,9 @@ public class SpanningTree extends BasicAlgorithm{
     public static final int STATUS_ROOT = 1;
     public static final int STATUS_FAULTY = 2;
     public static final int HEARTBEAT_INTERVAL = 5;
-
     // Maximum Delay between levels
     int MIN_LEVEL_DELAY = 1;
     int MAX_LEVEL_DELAY = 2;
-
     // Caption Next to Node
     String caption = "<0>→<0, 0, 0>";
     // Color of the Node - Default Black
@@ -28,10 +26,8 @@ public class SpanningTree extends BasicAlgorithm{
     Color color = defaultColor;
     int nodeStatus = STATUS_DEFAULT;
     boolean heartbeatReceived;
-
     // Random number generator, used for arbitrary failure
     Random generator = new Random();
-    
     // Relational Information
     int id = 0;
     int rootNode = 0;
@@ -41,8 +37,7 @@ public class SpanningTree extends BasicAlgorithm{
     /**
      * Setup the algorithm
      */
-    public void setup(java.util.Map<String, Object> config)
-    {
+    public void setup(java.util.Map<String, Object> config) {
 	// Setup our IDs
 	id = (Integer) config.get("node.id");
 	// Default to ourselves as root
@@ -105,7 +100,6 @@ public class SpanningTree extends BasicAlgorithm{
 	    message.rootNode = id;
 	    sendAll(message);
 	}
-
 	// We've encountered a timeout as not Root, this means
 	// messages have not reached us in enough time, the network
 	// is possibly broken!!!
@@ -117,7 +111,6 @@ public class SpanningTree extends BasicAlgorithm{
 	    message.rootNode = id;
 	    message.treeLevel = 0;
 	    sendAll(message);
-
 	} else {
 	    // Reset heartbeatRecieved Flag for next iteration
 	    heartbeatReceived = false;
